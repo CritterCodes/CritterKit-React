b4e2d462e3e931df327080b3bde091230d9817e9
CritterCodes <jacobaengel55@gmail.com>
2024-12-09 12:05:11 -0600

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.js', // Entry point of your library
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), terser()],
+  external: ['react', 'react-dom'], // Prevent bundling React
+};
